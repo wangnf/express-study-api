d505de1dd1266548fa17c78def1514ba71067d9b
宁飞 <w990752800@gmail.com>
2023-03-26 21:23:25 +0800

    feat: 添加routes，controllers

---
+++ controllers/auth.js
@@ -0,0 +1,11 @@
+export const register = (req, res) => {
+  res.send("register");
+};
+
+export const login = (req, res) => {
+  res.send("login");
+};
+
+export const logout = () => {
+  res.send("logout");
+};
--- index.js
+++ index.js
@@ -1,8 +1,13 @@
-import express from 'express';
-
+import express from "express";
 
 const app = express();
 
+import userRoutes from "./routes/users.js";
+import authRoutes from "./routes/auth.js";
+
+app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
+
 app.listen(8800, () => {
-    console.log('api is working!');
-})
+  console.log("api is working!");
+});
