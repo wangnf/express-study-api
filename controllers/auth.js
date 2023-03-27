80720876f0c8e24de184910b3b6d7f90a80fccee
宁飞 <w990752800@gmail.com>
2023-03-27 19:54:48 +0800

    feat: 修改配置文件

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,13 +1,14 @@
 import db from "../connect.js";
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 export const register = (req, res) => {
   const q = "select * from users where username = ?";
   db.query(q, [req.body.username], (err, data) => {
     if (err) return res.status(500).json(err);
     if (data.length) return res.status(409).json("用户已存在！");
-    const salt = bcrypt.genSaltSync(10);
 
+    const salt = bcrypt.genSaltSync(10);
     const hashedPassword = bcrypt.hashSync(req.body.password, salt);
 
     const q =
@@ -29,9 +30,39 @@ export const register = (req, res) => {
 };
 
 export const login = (req, res) => {
-  res.send("login");
+  const q = "select * from users where username = ?";
+
+  db.query(q, [req.body.username], (err, data) => {
+    if (err) return res.status(500).json(err);
+
+    if (data.length === 0) return res.status(404).json("用户不存在！");
+
+    const checkPassword = bcrypt.compareSync(
+      req.body.password,
+      data[0].password
+    );
+    if (!checkPassword) return res.status(400).json("用户名或密码不正确！");
+
+    // cookie
+    const token = jwt.sign({ id: data[0].id }, "secretkey");
+
+    const { password, ...others } = data[0];
+
+    res
+      .cookie("accessToken", token, {
+        httpOnly: true,
+      })
+      .status(200)
+      .json(others);
+  });
 };
 
-export const logout = () => {
-  res.send("logout");
+export const logout = (req, res) => {
+  res
+    .clearCookie("accessToken", {
+      secure: true,
+      sameSite: "none",
+    })
+    .status(200)
+    .json("已登出！");
 };
--- index.js
+++ index.js
@@ -1,15 +1,22 @@
 import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import { PORT } from './config.js'
 
 const app = express();
 
 import userRoutes from "./routes/users.js";
 import authRoutes from "./routes/auth.js";
 
+
+// middlewares
 app.use(express.json());
+app.use(cors());
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 
-app.listen(8800, () => {
+app.listen(PORT, () => {
   console.log("api is working!");
 });
